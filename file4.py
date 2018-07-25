182f9f72e3328051b819019fce291ffa97bfbc86
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:23:03 -0400

    added a file to convert dollar to INR

---
+++ file4.py
@@ -0,0 +1,5 @@
+rate=float(input("Enter the current dollar rate:"))
+dollar=int(input("Enter the number of dollars to be converted to rupee:"))
+INR=dollar*rate
+print("The INR for ",dollar)
+print("dollar is ",INR)
