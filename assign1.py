ab95ffc2855829807e4fe3227400a0274f0cfaad
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-30 04:13:27 -0400

    modified script to check whether file exists or not

--- assign1.py
+++ assign1.py
@@ -1,6 +1,2 @@
-fhand=open("/etc/resolv.conf")
-data=fhand.read()
-if data:
-    print("the file Exists")
-else :
-    print("The file doesnot Exist")
+import os
+print(os.path.isfile('/etc/resolv.conf'))
